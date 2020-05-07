81753e3f08a82b9f94855efae7ac7023d0a9ed35
Christian Cumberlander <christiancumberlander@ChristiansMBP4.attlocal.net>
2020-05-07 19:42:23 -0400

    Add Dice Class

---
+++ diceroll2.py
@@ -0,0 +1,10 @@
+from random import randint
+
+class DiceRoll(object):
+    def __init__(self, object)
+        self.min_roll = 1
+        self.max_roll = 6
+
+    def random_roll()
+        return randint(self.min_roll, self.max_roll)
+        
