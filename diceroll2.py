2ccf064529d13064364fc5423ed9f0e315418714
Christian Cumberlander <christiancumberlander@ChristiansMBP4.attlocal.net>
2020-05-08 15:17:11 -0400

    Add 2 dice objects + total for both

--- diceroll2.py
+++ diceroll2.py
@@ -9,6 +9,13 @@ class DiceRoll(object):
     def random_roll(self):
         return randint(self.min_roll, self.max_roll)
 
-TheMan = DiceRoll('TheMan')
-print(TheMan.__dict__)
-print(TheMan.random_roll())
+# TheMan = DiceRoll('TheMan')
+# print(TheMan.__dict__)
+# print(TheMan.random_roll())
+
+dice1 = DiceRoll('dice1')
+dice2 = DiceRoll('dice2')
+
+total = dice1.random_roll() + dice2.random_roll()
+
+# print(total)
