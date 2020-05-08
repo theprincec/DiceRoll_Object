9f9823a43536c8601e4ec17aa21fbb322a926f6d
Christian Cumberlander <christiancumberlander@ChristiansMBP4.attlocal.net>
2020-05-08 15:11:40 -0400

    Class correction + roll function

--- diceroll2.py
+++ diceroll2.py
@@ -1,10 +1,14 @@
 from random import randint
 
 class DiceRoll(object):
-    def __init__(self, object)
+    def __init__(self, object):
+        self.dice_name = object
         self.min_roll = 1
         self.max_roll = 6
 
-    def random_roll()
+    def random_roll(self):
         return randint(self.min_roll, self.max_roll)
-        
+
+TheMan = DiceRoll('TheMan')
+print(TheMan.__dict__)
+print(TheMan.random_roll())
