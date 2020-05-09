b4cb656b24b7c8b66ab89baa7f6192aff46557e8
Christian Cumberlander <christiancumberlander@ChristiansMBP4.attlocal.net>
2020-05-09 14:10:45 -0400

    Add game counter

--- diceroll2.py
+++ diceroll2.py
@@ -1,22 +1,25 @@
 from helpers import *
 
-name = input('what is your name?')
+name = input('what is your name? ')
 # status = input(f'hi {name} thanks for playing')
 
 dice1 = DiceRoll('dice1')
 dice2 = DiceRoll('dice2')
-
-game_status = input(f'{name}, Press a key to roll the dice')
+counter = 0
+game_status = input(f'{name}, Press a key to roll the dice ')
 while game_status is not 'q':
+    # counter == 0
     total_roll = dice1.random_roll() + dice2.random_roll()
     # total_roll = 11
     print(total_roll)
     if total_roll == 11:
+        counter += 1
         print(f"SNAKE EYES. You're a winner, {name}!")
     elif (total_roll == 2) or (total_roll == 3) or (total_roll == 12) :
         print('Craps!')
     else:
         pass
+    print(f'games won: {counter}')
     game_status = input(f"{name}, Press any key to roll the dice. Press 'q' to quit")
 # print(f'{name} press a key to play.')
 # print(name)
