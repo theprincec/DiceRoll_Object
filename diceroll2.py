fe8821a00541846e920cee0335a8a880fafbc962
Christian Cumberlander <christiancumberlander@ChristiansMBP4.attlocal.net>
2020-05-09 14:05:12 -0400

    add conditionals to loop

--- diceroll2.py
+++ diceroll2.py
@@ -9,7 +9,14 @@ dice2 = DiceRoll('dice2')
 game_status = input(f'{name}, Press a key to roll the dice')
 while game_status is not 'q':
     total_roll = dice1.random_roll() + dice2.random_roll()
+    # total_roll = 11
     print(total_roll)
+    if total_roll == 11:
+        print(f"SNAKE EYES. You're a winner, {name}!")
+    elif (total_roll == 2) or (total_roll == 3) or (total_roll == 12) :
+        print('Craps!')
+    else:
+        pass
     game_status = input(f"{name}, Press any key to roll the dice. Press 'q' to quit")
 # print(f'{name} press a key to play.')
 # print(name)
